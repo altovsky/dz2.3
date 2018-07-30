c33c132910e0dbadd98d6c9a9b2be679a581835f
altovsky <41269058+altovsky@users.noreply.github.com>
2018-07-31 02:53:34 +0300

    add

--- dz2.3.py
+++ dz2.3.py
@@ -2,40 +2,55 @@
 
 import json
 # import xml.etree.ElementTree as ET
-# import re
 import os
 
 file_format = 'json'
 # file_format = 'xml'
+min_number_of_letters = 6
+rate_threshold = 10
+
+
+def read_file_content(file_name_value, encoding_type):
+    with open(file_name_value, encoding=encoding_type, mode='r') as input_file:
+        return json.load(input_file)
+
+
+def extract_counted_news(file_data):
+    news_list = file_data['rss']['channel']['items']
+    words_rated_dict = dict()
+    for news in news_list:
+        news_line = news['description']
+        news_word_list = news_line.split(' ')
+
+        for word_val in news_word_list:
+            if len(word_val) > min_number_of_letters:
+                if word_val not in words_rated_dict.keys():
+                    words_rated_dict[word_val] = 1
+                else:
+                    words_rated_dict[word_val] += 1
+    return words_rated_dict
+
+
+def find_all_values(word_val, word_dictionary):
+    rate_val = word_dictionary[word_val]
+    word_list = []
+    for word_in_dict in word_dictionary:
+        if word_dictionary[word_in_dict] == rate_val and word_in_dict not in word_list:
+            word_list.append(word_in_dict)
+    return word_list
+
 
 file_list = os.listdir(os.path.dirname(os.path.abspath(__file__)))
 
-# words_rating = dict()
-
+print('Tоп {} самых часто встречающихся в '
+      'новостях слов длиннее {} символов.\n'.format(rate_threshold, min_number_of_letters))
 for file_name in file_list:
     if file_name[len(file_name) - len(file_format):len(file_name)] == file_format:
-        print(file_name, '\n')
-        with open(file_name, encoding='utf-8', mode='r') as input_file:
-            file_data = json.load(input_file)
-            news_list = file_data['rss']['channel']['items']
-            news_word_list = []
-            words_rating = dict()
-            for news in news_list:
-                news_line = news['description']
-                news_word_list = news_line.split(' ')
-
-                for word in news_word_list:
-                    if len(word) > 6:
-                        if word not in words_rating.keys():
-                            words_rating[word] = 1
-                            # print('!!!')
-                        else:
-                            words_rating[word] += 1
-                            # print('&')
-
-            news_word_sorted_list = sorted(news_word_list)
-            print(news_word_sorted_list)
-            print(words_rating)
-            for word in words_rating.keys():
-                if words_rating[word] > 13:
-                    print('Key: {} Val: {}'.format(word, words_rating[word]))
+        file_content = read_file_content(file_name, 'utf-8')
+        news_dict = extract_counted_news(file_content)
+        words_sorted = sorted(news_dict, key=lambda x: news_dict[x], reverse=True)
+        words_rated_sorted = words_sorted[0:rate_threshold]
+        print('В файле {}: '.format(file_name))
+        for word in words_rated_sorted:
+            print('{} - {}'.format(word, news_dict[word]))
+        print('')
